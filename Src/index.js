7c685ebf5ce67c0afafa73132e11d3e2880e4c3d
Kalpa Wijesooriya <kalpa.wijesooriya94@gmail.com>
2021-06-06 00:16:24 +0530

    port changed

--- Src/index.js
+++ Src/index.js
@@ -6,7 +6,7 @@ require('./db/mongoose')
 const catRouter = require('./routers/cats')
 const app =express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 app.use(express.json())
 
 
