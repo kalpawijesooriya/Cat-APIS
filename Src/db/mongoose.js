b19829118f90b24c3369f3a5c65c1fdc5a7ba343
kalpa wijesooriya <kalpa.wijesooriya@yahoo.com>
2021-06-06 02:01:45 +0530

    connection string changed

--- Src/db/mongoose.js
+++ Src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/cats',{
+mongoose.connect('mongodb://dbcats:Zx6925Ufw4kTpSuAlnmHKbuoVYvgYZcLFiLprYHvvsXvDSMbf6JvYmR3MeElXtmiMP1nRdESY1fVwhTi7HDbyQ%3D%3D@dbcats.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@dbcats@',{
     userNewUrlparse:true,
     UserCreateIndex:true
 })
